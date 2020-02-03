3209860e19719c6f3ae11d3e394836f6b0b4b437
lofcek <lofcek@gmail.com>
2020-02-03 15:19:03 +0100

    challenge 3 and 4 done

---
+++ 4/ThreadExample4.cpp
@@ -0,0 +1,80 @@
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <random>
+#include <sstream>
+#include <thread>
+#include "../1/LockGuard.h"     // this is a bit ugly include, but I do not want to copy&paste code
+
+
+// comments:
+// 1. I didn't commit challenge 3, however directly challenge 4. To achieve 3 just replace `condition_variable_any` to `condition_variable` and `challenge::lock_guard` to `std::lock_guard`.
+// 2. `std::condition_signal` do not exist. Probably it was typo, correct should be `std::condition_variable`.
+// 3. Synchronization primitive condition_variable does not allow to specify which thread will be notified (woken up).
+//    Therefore there are two possibilities solve the issue.
+//     - All threads will be woken by `notify_all`. Disadvantage of this issue will be for bigger amount of threads.
+//     - To have specificy condition_variable and mutex for each thread.
+
+using namespace std;
+static std::mutex g_mutex;
+static std::condition_variable_any g_cond_var;
+static std::string g_current;
+static int g_ready = 0;
+
+static std::random_device g_rd;  //Will be used to obtain a seed for the random number engine
+static std::mt19937 g_gen(g_rd());
+
+void thread_main(std::string me, std::string next_thread) {
+    std::uniform_int_distribution<int> timeout(1000, 5000);
+
+    challenge::lock_guard lk(g_mutex);
+    g_ready++;
+    g_cond_var.notify_all();
+
+    cout << me << ": starting, waiting.\n";
+    for(;;) {
+        g_cond_var.wait(g_mutex, [&]{return g_current == me;});
+        cout << me << ": signal received, doing work ....\n";
+        this_thread::sleep_for(std::chrono::milliseconds(
+            timeout(g_gen)
+        ));
+        cout << me << ": done with work, signal next thread\n";
+        g_current = next_thread;
+        g_cond_var.notify_all();
+
+        // here is a small pitfall, because mutex cannot be released at the end of the loop.
+        // If mutex had been released here, the g_cond_var could have been notified sooner than
+        // this thread would be waiting. Notifiacition could be lost (lost wakeup).   
+    }
+}
+
+int main() {
+    // names of threads
+    const char* n1 = "thread1";
+    const char* n2 = "thread2";
+    const char* n3 = "thread3";
+
+    cout << "main: starting all threads\n";
+    thread t1(thread_main, n1, n2);
+    thread t2(thread_main, n2, n3);
+    thread t3(thread_main, n3, n1);
+
+    // and here is another pitfall. I should not notify the first thread before all threads
+    // are wainting. But I how I know that. I decide to use variable g_ready and the same g_cond_var
+    // to notify also main, that all threads are waiting.
+    {
+        challenge::lock_guard lk(g_mutex);
+        g_cond_var.wait(g_mutex, [&]{return g_ready == 3;});
+    }
+
+    // I should wait until all threads are waiting.
+    cout <<  "main: starting thread 1.\n";
+    g_current = n1;
+    g_cond_var.notify_all();
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Getting sources
+
+`git clone --recursive https://github.com/lofcek/onsemi.git`
+
+## Building
+
+Please install ninja. Building should be executed by command `ninja`. Compiled binaries are stored in directory `binaries` and temporary files (like *.o) are stored in `build`.
